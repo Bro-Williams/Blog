8d969bce4b078f01a7244327d5df5c41d429748a
Bro-Williams <ad-gloriam@mail.ru>
2021-06-08 17:01:39 +0300

    enabled templates

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'posts/index.html')
+
+
+def post(request):
+    return render(request, 'posts/post.html')
