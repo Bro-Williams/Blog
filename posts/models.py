3c9fbcb9e0268bdc50fd2d2b93fc4ea0f7abec81
Bro-Williams <ad-gloriam@mail.ru>
2021-06-09 17:08:10 +0300

    added model in admin panel

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=64)
+    text = models.TextField()
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
