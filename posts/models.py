6c0453e1a4bf8bb785c25b70f0a44f6523a68908
Bro-Williams <ad-gloriam@mail.ru>
2021-06-10 17:59:01 +0300

    added new migrations. Added posts

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,10 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
-    description = models.CharField(max_length=64)
+    description = models.CharField(max_length=128, blank=True)
     text = models.TextField()
     created_timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
